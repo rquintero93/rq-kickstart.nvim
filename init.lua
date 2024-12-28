3e1e2f0c47ef152586549f78b8ca9ac30a384fd1
Ricardo Quintero <rquinterog93@gmail.com>
2024-12-28 19:53:39 +0100

    still trying to get bq to work

--- init.lua
+++ init.lua
@@ -855,7 +855,7 @@ require('lazy').setup({
     'kristijanhusak/vim-dadbod-ui',
     dependencies = {
       { 'tpope/vim-dadbod', lazy = true },
-      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql', 'bq' }, lazy = true },
     },
     cmd = {
       'DBUI',
